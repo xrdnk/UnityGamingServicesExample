0fd02f0abef19eae361a8d3c841413f970dc092f
xrdnk <xrdnk1010@gmail.com>
2021-10-24 23:27:03 +0900

    feat: UIViewBase 導入

---
+++ Assets/@UGSExample/Scripts/Shared/UIViewBase/UIViewBase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.EventSystems;
+
+namespace Shared.UIViewBase
+{
+    public abstract class UIViewBase : UIBehaviour
+    {
+        public virtual void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public virtual void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
